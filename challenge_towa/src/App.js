1f33ad12db1b4fb5ce5975fe6a28066951bf1804
Leandro Rossi Sampaio <leandro.r.sampaio@gmail.com>
2023-05-10 13:43:46 +0200

    Installing Swiper
    - Installing Swiper in the project
    - Adding dummy content
    - Adding imports (css and react)
    - Adding temp CSS for swiper

--- challenge_towa/src/App.js
+++ challenge_towa/src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper";
 import Headerpage from './objects/headerpage';
 import Footerpage from './objects/footerpage';
+
+// Swiper Styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 
 function App() {
 
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  };
 
   return (
     <>
@@ -12,6 +25,27 @@ function App() {
       <Headerpage />
 
       <h1>Hellow World</h1>
+
+      <Swiper navigation={true} modules={[Navigation, Pagination]} pagination={pagination} className="mySwiper">
+        <SwiperSlide>
+          <img src="https://source.unsplash.com/random?landscape,mountain" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://source.unsplash.com/random?landscape,night" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://source.unsplash.com/random?landscape,city" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://source.unsplash.com/random?landscape,cars" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://source.unsplash.com/random?landscape,dog" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://source.unsplash.com/random?landscape,cat" alt="" />
+        </SwiperSlide>
+      </Swiper>
 
       <Footerpage />
 
